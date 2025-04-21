d67946fa2208d304fd8a007ef0bc68ba9ab59898
apinlight <apin99lightning@gmail.com>
2025-04-21 11:18:22 +0700

    Initial Commit (Connected to Backend)

---
+++ src/components/ApiTest.vue
@@ -0,0 +1,25 @@
+//ApiTest.vue
+<template>
+    <div>   
+        <h1>Api Test</h1>
+        <button @click="testApi">Check Connection to Backend</button>
+        <p v-if="message">{{ message }}</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import api from '@/services/api'
+
+const message = ref('')
+
+const testApi = async () => {
+  try {
+    const response = await api.get('/')
+    message.value = response.data.message
+  } catch (error) {
+    message.value = `Error: Unable to connect to backend > ${error}`
+  }
+}
+
+</script>
